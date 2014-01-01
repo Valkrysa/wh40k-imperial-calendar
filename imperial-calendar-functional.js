7a4f39bf682cb5b1025cbba484f431b3370a064d
Valkrysa <valkrysa@gmail.com>
2014-01-01 10:23:11 -0800

    Included @fabsh 's precision improvement
    This adds the precision improvement to the functional version.

--- imperial-calendar-functional.js
+++ imperial-calendar-functional.js
@@ -21,17 +21,35 @@ function imperialCalendar(aDate, aWithSpaces, aCheckNumber) {
 	
 	var theImperialDate = "";
 	
+	// The gets the hours, minutes and seconds of the current date
+
+	var currentDate = new Date();
+	var currentHour = currentDate.getHours();
+	var currentMinute = currentDate.getMinutes();
+	var currentSecond = currentDate.getSeconds();
+	
 	var theStart = new Date(theStandardDate.getFullYear(), 0, 0);
 	var theDiff = theStandardDate - theStart;
 	var theDay = Math.floor(theDiff / (1000 * 60 * 60 * 24));
-	var theDaysInYear = 365;
+	
+	// Here we're getting the current elapsed seconds in the year for the days, hours and minutes
+	// Substracting one day's worth of seconds to make the date start counting at zero
+	var days = (theDay * 86400) - 86400
+	var hours = currentHour * 3600
+	var minutes = currentMinute * 60
+
+
+	// There are this many seconds in a year
+	var secondsInYear = 31536000;
 	
 	var isLeap = new Date(theStandardDate.getFullYear(), 1, 29).getMonth() == 1
         if (isLeap) {
-                theDaysInYear = 366;
+                secondsInYear = 31622400;
         }
         
-        var theYearFraction = Math.ceil((theDay/theDaysInYear) * 1000);
+        // Add all the currently elapsed seconds together, divide by seconds in the year
+	// Then multiply by a thousand and round it all down to get the current year fraction
+	var theYearFraction = Math.floor(((days + hours + minutes + currentSecond) / secondsInYear) * 1000);
 	
 	//quick and dirty padding for our limited cases
 	if (theYearFraction < 10) {
